5de717952cc72655de6329d572c9d01e1f426149
elhadjibabacarsene <elhadjibabacarsene@gmail.com>
2020-05-28 14:56:14 +0000

    test

--- saquizz_db/config/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-//CONSTANTE QUI NOUS PERMET D'ACCEDER A L'URL SOURCE
-define("URL",str_replace("index.php","", (isset($_SERVER["HTTPS"])? "https" : "http"). "://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]"));
--- saquizz_db/controllers/frontend.controllers.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'config/config.php';
-
-function getPageConnexion(){
-    $title = 'Se connecter';
-
-    require_once 'views/frontend/login.view.php';
-}
